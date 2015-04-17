3adbb9cf2ffe081acca34b66097136348458104e
Franciszek Jemioło <f.jemiolo@gmail.com>
2015-04-17 12:28:38 +0200

    Some rubbish files
    Very alpha project

---
+++ Cast into the Voidstar/Assets/GameC.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameC : MonoBehaviour {
+
+	void Start () {
+		this.navigateTo (GameObject.FindGameObjectsWithTag ("Unit"), new Vector3 (100, 0, 100));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Moving one unit into position.
+	public void navigateTo (GameObject unit, Vector3 pos)
+	{
+		NavMeshAgent navAgent;
+		navAgent = unit.GetComponent<NavMeshAgent> ();
+		navAgent.SetDestination (pos);
+
+	}
+
+	// Moving many units to position.
+	public void navigateTo (GameObject[] units, Vector3 pos)
+	{
+		foreach (GameObject unit in units) {
+			navigateTo(unit, pos);
+		}
+	}
+}
---
+++ Cast into the Voidstar/Assets/scr1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scr1 : MonoBehaviour {
+	
+	private Button mybut;
+	// Use this for initialization
+	void Start () {
+		mybut = this.GetComponent<Button> ();
+		GameC gc = FindObjectOfType<GameC> ();
+		mybut.onClick.AddListener (() => {
+			gc.navigateTo (GameObject.FindGameObjectWithTag ("Unit"), new Vector3 (0, 0, 50));});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+}
