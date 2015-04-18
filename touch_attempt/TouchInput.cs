472cfd9e53f12f47a25eaa48fe18165755e2ba68
perpetual-fsm <daniel.pmhp.le@gmail.com>
2015-04-18 16:03:24 +0200

    added swipe
    added swipe, early attempt to implement resize camera

--- touch_attempt/TouchInput.cs
+++ touch_attempt/TouchInput.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class TouchInput : MonoBehaviour {	
 	// Update is called once per frame
 	RaycastHit hit;
+	private float distanceMoved = 0;
 
 	void Update () {
-
-		foreach (Touch touch in Input.touches) {
-
-			Debug.Log("before touchcount, after foreach");
-			//single touch - detect taps (select) and drags (move camera)
-			if(Input.touchCount==1){
+		//single touch - detect taps (select) and drags (move camera)
+		switch(Input.touchCount) {
+		case 1:
+			Touch touch = Input.GetTouch(0);
+			switch(touch.phase) {
+			case TouchPhase.Began:
 				Ray ray1 = Camera.main.ScreenPointToRay(touch.position);
-				Debug.Log("before hit");
+				Debug.Log("touchphase began");
 				// we have to detect what we are hitting - it must be unit(s)
 				if(Physics.Raycast(ray1, out hit, 1000)){
 					GameObject recipient = hit.transform.gameObject;
@@ -21,18 +22,36 @@ public class TouchInput : MonoBehaviour {
 					//if( recipient.GetType() == typeof(Unit) )
 					recipient.SendMessage("select", recipient, SendMessageOptions.DontRequireReceiver);
 				}
-
+				break;
+			case TouchPhase.Moved:
+				distanceMoved += Mathf.Abs(touch.deltaPosition.x) + Mathf.Abs(touch.deltaPosition.y);
+				if( distanceMoved>10 ){
+					Debug.Log("swipe");
+				}
+				break;
+			case TouchPhase.Ended:
+				distanceMoved = 0;
+				Debug.Log ("Touchphase ended");
+				break;
+			}
+			break;
+		//resize camera (move by Y axis)
+		case 2:
+			Touch touch1 = Input.GetTouch(0);
+			Touch touch2 = Input.GetTouch(1);
+			if( touch1.phase==TouchPhase.Began && touch2.phase==TouchPhase.Began ){
 
 			}
-
-			//resize camera (move by Y axis)
-			else if(Input.touchCount==2){
+			else if( touch1.phase==TouchPhase.Moved || touch2.phase==TouchPhase.Moved ){
 
 			}
-			//selecting groups
-			else if(Input.touchCount==3){
+			else if( touch1.phase==TouchPhase.Ended || touch2.phase==TouchPhase.Ended ){
 
 			}
+			break;
+		//selecting groups
+		case 3:
+			break;
 		}
 	}
 }
