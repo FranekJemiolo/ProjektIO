a1e142a45655850cda09efee64136e79e8904ae8
Franciszek Jemioło <f.jemiolo@gmail.com>
2015-04-19 22:39:05 +0200

    Fully functional capping of asteroid.
    It's fully functional code for asteroid capping. Next thing to do is to
    give points for captured asteroid and main game variables.

--- src_code/AsteroidController.cs
+++ src_code/AsteroidController.cs
@@ -3,8 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AsteroidController : MonoBehaviour {
-
-	public float timeCapped;
+	
 	// The range from which we can capture an asteroid, square size.
 	public float captureRangeSqr = 4900.0f;
 
@@ -19,6 +18,10 @@ public class AsteroidController : MonoBehaviour {
 	// Who is currently capping.
 	private Master capping;
 
+	// Time from cap start.
+	private float timeCapped = 0.0f;
+	public float cappingTime = 5.0f;
+
 
 	// Use this for initialization
 	void Start () 
@@ -32,62 +35,103 @@ public class AsteroidController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		// Check if someone is capping? Set to none if no one.
+		if ((units.Count == 0) && (enemies.Count == 0))
+		{
+			capping = Master.None;
+			timeCapped = 0.0f;
+		}
+
 	}
 
 	void OnTriggerEnter (Collider other) 
 	{
 		Debug.Log (other + "Entering" + other.gameObject.tag);
-		Master whoAmI = Master.None;
 		if (other.gameObject.tag == "Enemy") 
 		{
 			enemies.Add (other.gameObject);
-			whoAmI = Master.Enemy;
 		} 
 		else if (other.gameObject.tag == "Unit") 
 		{
 			units.Add (other.gameObject);
+		}
+
+	}
+	void OnTriggerStay (Collider other) 
+	{
+		Master whoAmI = Master.None;
+		if (other.gameObject.tag == "Enemy") 
+		{
+			whoAmI = Master.Enemy;
+		} 
+		else if (other.gameObject.tag == "Unit") 
+		{
 			whoAmI = Master.Player;
 		}
-		// If no one holds the asteroid. Also TO DO: time passed to cap etc.
+		// If no one holds the asteroid.
+		if (timeCapped > cappingTime)
+		{
+			if ((capping == whoAmI) && (capping != Master.None))
+			{
+				belongsTo = whoAmI;
+			}
+		}
 		if (belongsTo == Master.None)
 		{
 			// If no one currently capping the asteroid.
 			if (capping == Master.None)
 			{
 				capping = whoAmI;
+				timeCapped += Time.deltaTime;
 			}
 			else if (capping != whoAmI)
 			{
 				capping = Master.None;
+				timeCapped = 0.0f;
 			}
-			// Else do nothing because capping == whoAmI;
+			// Else do nothing because capping == whoAmI.
+			else
+			{
+				timeCapped += Time.deltaTime;
+			}
+			
 		}
 		else if (belongsTo != whoAmI)
 		{
 			if (whoAmI == Master.Player)
 			{
-				// No enemies we can convert for us.
+				// No enemies, we can convert astero for us.
 				if (enemies.Count == 0)
 				{
 					capping = whoAmI;
+					timeCapped += Time.deltaTime;
 				}
-				// else we must kill them.
+				// else we must kill them. No cap.
+				else
+				{
+					capping = Master.None;
+					timeCapped = 0.0f;
+				}
 			}
 			else if (whoAmI == Master.Enemy)
 			{
-				// No enemies we can convert for us.
-				if (enemies.Count == 0)
+				// No enemies(units), we can convert for us.
+				if (units.Count == 0)
 				{
 					capping = whoAmI;
+					timeCapped += Time.deltaTime;
 				}
-				// else we must kill them.
+				// else we must kill them. No cap.
+				else
+				{
+					capping = Master.None;
+					timeCapped = 0.0f;
+				}
 			}
 		}
+		Debug.Log (capping);
+		Debug.Log(belongsTo);
 		// Else do nothing we already have it.
-	}
-	void OnTriggerStay (Collider other) 
-	{
-		//Debug.Log ("Staying" + units);
 	}
 	void OnTriggerExit (Collider other) 
 	{
@@ -95,10 +139,20 @@ public class AsteroidController : MonoBehaviour {
 		if (other.gameObject.tag == "Enemy") 
 		{
 			enemies.Remove (other.gameObject);
+			/*if (enemies.Count == 0)
+			{
+				capping = Master.None;
+				timeCapped = 0.0f;
+			}*/
 		} 
 		else if (other.gameObject.tag == "Unit") 
 		{
 			units.Remove (other.gameObject);
+			/*if (units.Count == 0)
+			{
+				capping = Master.None;
+				timeCapped = 0.0f;
+			}*/
 		}
 	}
 }
