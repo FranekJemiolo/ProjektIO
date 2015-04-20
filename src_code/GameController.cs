45241d2a2832a37c2de2a78a6887b4aa6f7afa78
Franciszek Jemioło <f.jemiolo@gmail.com>
2015-04-20 23:25:39 +0200

    Added player variables class.
    Player variables class stores all the important data of a player such as
    references to the objects, points. And there will probably be more to it
    later.

--- src_code/GameController.cs
+++ src_code/GameController.cs
@@ -1,8 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+// This class is used to represent players in-game variables.
+public class Player
+{
+	// Points to victory of a player.
+	private float points;
+
+	// Now will be the list of units of some types.
+	private enum UnitType {Mothership = 0, Destroyer, Cruiser, Fighter, Transporter, Shuttle, Unknown};
+	private const int typeOfUnits = 7;
+	private List<GameObject>[] units;
+
+	Player () 
+	{
+		List<GameObject>[] units = new List<GameObject>[typeOfUnits];
+	}
+
+	// Unit type translation.
+	private UnitType getUnitType (GameObject ob)
+	{
+		if (ob.name == "Mothership")
+		{
+			return UnitType.Mothership;
+		}
+		else if (ob.name == "Destroyer")
+		{
+			return UnitType.Destroyer;
+		}
+		else if (ob.name == "Cruiser")
+		{
+			return UnitType.Cruiser;
+		}
+		else if (ob.name == "Fighter")
+		{
+			return UnitType.Fighter;
+		}
+		else if (ob.name == "Transporter")
+		{
+			return UnitType.Transporter;
+		}
+		else if (ob.name == "Shuttle")
+		{
+			return UnitType.Shuttle;
+		}
+		else
+		{
+			return UnitType.Unknown;
+		}
+	}
+
+	// Some methods for accessing fields.
+
+	// Should be called when creating new object for a player.
+	public void addUnit(GameObject ob)
+	{
+		UnitType t = getUnitType(ob); 
+		units[(int)t].Add(ob);
+	}
+
+	// Should be called on destroying an object.
+	public void removeUnit(GameObject ob)
+	{
+		UnitType t = getUnitType(ob); 
+		units[(int)t].Add(ob);
+	}
+
+
+	public void setPoints (float p)
+	{
+		this.points = p;
+	}
+
+	public float getPoints ()
+	{
+		return this.points;
+	}
+
+}
+
 
 public class GameController : MonoBehaviour 
 {
+	// Space for game variables.
+	private const int numberOfPlayers = 2;
+	Player[] players = new Player[numberOfPlayers];
+
+	// End of game variables.
 	private bool first = false;
 	private float ten = 0.0f;
 	void Start () 
@@ -13,6 +98,7 @@ public class GameController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		// Debug test.
 		if (!first)
 		{
 			//this.navigateTo (GameObject.FindGameObjectWithTag ("Unit"), new Vector3 (50, 0, 100));
@@ -21,6 +107,7 @@ public class GameController : MonoBehaviour
 		}
 		ten++;
 		//this.moveCamera(new Vector3(this.transform.position.x+(ten/10),this.transform.position.y,this.transform.position.z+(ten/10)));
+		// End of debug test.
 	}
 
 	// Moving one unit into position.
@@ -53,6 +140,7 @@ public class GameController : MonoBehaviour
 		}
 	}
 
+	// Moves main camera into desired position.
 	public void moveCamera(Vector3 pos)
 	{
 		this.transform.position = pos;
