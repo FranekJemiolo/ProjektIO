b429f67fece03a7be83e9fc85438e6fe7afaeda1
kkragoth <kkragoth@gmail.com>
2015-04-21 11:48:27 +0100

    GUI class
    Started working on GUI class

---
+++ src_code/Gui.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ui : MonoBehaviour {
+
+	public Text hullStatus;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hullStatus.text = "1" + "%";
+	
+	}
+}
