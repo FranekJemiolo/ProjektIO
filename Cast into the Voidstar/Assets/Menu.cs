2371904389e6029c37b6fbb7b7fee9aed4fc66d8
kkragoth <kkragoth@gmail.com>
2015-04-28 14:41:04 +0100

    Main Menu
    Working prototype of main menu

---
+++ Cast into the Voidstar/Assets/Menu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	
+	public GameObject QuitPrompt;
+	public GameObject OptionsPrompt;
+
+	public void Exit() {
+		Debug.Log ("Quit");
+		Application.Quit();
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+        
+    }
+    
+    public void startDebugLevel() {
+		Application.LoadLevel("joystick");
+	}
+
+
+	public void showQuitPrompt() {
+		QuitPrompt.SetActive(true);
+	}
+
+	public void showOptionsPrompt() {
+		OptionsPrompt.SetActive(true);
+	}
+
+	public void closeOptionsPrompt() {
+		OptionsPrompt.SetActive(false);
+	} 
+
+	public void closeQuitPrompt() {
+		QuitPrompt.SetActive(false);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		QuitPrompt.SetActive(false);
+		OptionsPrompt.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- src_code/Gui.cs
+++ src_code/Gui.cs
@@ -4,16 +4,37 @@ using System.Collections;
 
 public class ui : MonoBehaviour {
 
-	public Text hullStatus;
+	public CNAbstractController MovementJoystick;
+	private Text hullStatus;
+	private Movement mv;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
+	private void drawHullStatus() {
+		hullStatus.text = "100" + "%";
+	}
+
+	private void handleJoystickMovement() {
+		mv = MonoBehaviour.FindObjectOfType<Movement>();
+
+		if (MovementJoystick.GetAxis("Horizontal") > 0.5) {
+			mv.rotateRight();
+		} else if (MovementJoystick.GetAxis("Horizontal") < - 0.5) {
+			mv.rotateLeft();
+		} else if (MovementJoystick.GetAxis("Vertical") > 0.5) {
+			mv.goForward();
+		} else if (MovementJoystick.GetAxis("Vertical") < -0.5) {
+			mv.goBack();
+		}
+
+	}
 	// Update is called once per frame
 	void Update () {
-		hullStatus.text = "1" + "%";
+		handleJoystickMovement();
+		drawHullStatus();
 	
 	}
 }
