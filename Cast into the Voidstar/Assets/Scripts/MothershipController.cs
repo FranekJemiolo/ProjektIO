04279436654e51bad3d735b608a8c7a3e6db1b70
Franciszek Jemioło <f.jemiolo@gmail.com>
2015-05-02 16:16:53 +0200

    Updated models etc.

--- Cast into the Voidstar/Assets/Scripts/MothershipController.cs
+++ Cast into the Voidstar/Assets/Scripts/MothershipController.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class MothershipController : MonoBehaviour {
-	
+
 	// Rotation speed in degrees.
 	private float rotateSpeed = 10.0f;
-	
+
 	// The speed and acceleration
 	private float speed = 10.0f;
 	private float sqrSpeed = 100.0f;
@@ -13,72 +13,83 @@ public class MothershipController : MonoBehaviour {
 	
 	// The life points of the unit.
 	private float hitPoints = 100.0f;
-	
+
 	// Number of attacks
 	public const int numberOfProjectiles = 2;
-	
+
 	// The firing rate of our unit. How many seconds between attacks.
 	private float[] firingRate = new float[numberOfProjectiles];
 	// How many time passed from last firing.
 	private float[] timePassed = new float[numberOfProjectiles];
-	
+
 	// The strength of the attack of the unit.
 	private float[] attackForce = new float[numberOfProjectiles];
-	
+
 	// The gameobject of a shot.
 	public GameObject[] projectiles = new GameObject[numberOfProjectiles];
 	
 	// The transform object where to spawn shots.
 	public Transform shotSpawn;
-	
+
 	// The rigidbody attached to the unit.
 	private Rigidbody myRigidbody;
 	
-	
+
 	void Start () 
 	{
 		this.myRigidbody = this.GetComponent<Rigidbody>();
 	}
-	
-	
+
+
 	void Update () 
 	{
-
+		for (int i = 0; i < numberOfProjectiles; i++)
+		{
+			timePassed[i] += Time.deltaTime;
+		}
+		//if (j == 10)
+		//{
+			rotateRight(Time.deltaTime);
+		//	j = 0;
+		//}
+		//j++;
+		moveForward(Time.deltaTime);
+		
 	}
 	
 	void FixedUpdate ()
 	{
 	}
-	
-	
+
+
 	// Rotates mothership to right.
 	public void rotateRight (float timeDelta)
 	{
 		this.transform.Rotate (0.0f, (this.rotateSpeed * timeDelta), 0.0f);
 	}
-	
+
 	// Rotates mothership to left.
 	public void rotateLeft (float timeDelta)
 	{
 		this.transform.Rotate (0.0f, -(this.rotateSpeed * timeDelta), 0.0f);
 	}
-	
+
 	// Moves mothership forward.
 	public void moveForward (float timeDelta)
 	{
-		
+
 		//if (this.myRigidbody.velocity.sqrMagnitude < sqrSpeed)
 		//{
-		Debug.Log(this.transform.forward);
-		//Vector3 res = this.transform.position - this.engines.transform.position;
-		//this.myRigidbody.AddForceAtPosition(this.transform.forward * this.acceleration * timeDelta, this.engines.transform.position);
+			Debug.Log(this.transform.forward);
+			//Vector3 res = this.transform.position - this.engines.transform.position;
+			//this.myRigidbody.AddForceAtPosition(this.transform.forward * this.acceleration * timeDelta, this.engines.transform.position);
 		Vector3 relativeVector = new Vector3 (Mathf.Abs(this.transform.forward.x), Mathf.Abs(this.transform.forward.y), Mathf.Abs(this.transform.forward.z));
 		this.myRigidbody.AddRelativeForce(relativeVector * this.acceleration * timeDelta);
 		//this.myRigidbody.AddForce(this.transform.forward * this.acceleration * timeDelta);
 		//}
 		// else we already reached max speed.
 	}
-	
+
 	// Moves mothership backwards.
 	public void moveBackward (float timeDelta)
 	{
@@ -89,7 +100,7 @@ public class MothershipController : MonoBehaviour {
 		//}
 		// else we reached max speed.
 	}
-	
+
 	// Fires one of mothership's attacks.
 	public bool fire (int whichAttack)
 	{
@@ -105,15 +116,15 @@ public class MothershipController : MonoBehaviour {
 		{
 			return false;
 		}
-		
+
 	}
-	
+
 	// Check if an attack is available?
 	public bool attackAvailable (int attackNumber)
 	{
 		return (timePassed[attackNumber] > firingRate[attackNumber]);
 	}
-	
+
 	// Returns time to when the wanted attack will be available.
 	public float getTimeToAttack (int attackNumber)
 	{
@@ -124,7 +135,7 @@ public class MothershipController : MonoBehaviour {
 		}
 		return res;
 	}
-	
+
 	
 	public float getAttackForce (int whichAttack)
 	{
@@ -135,27 +146,27 @@ public class MothershipController : MonoBehaviour {
 	{
 		return this.hitPoints;
 	}
-    
-    public void setHitPoints (float f)
-    {
-        this.hitPoints = f;
-    }
-    
-    // Called when unit reach below 0 hp.
-    // Destroys object and does some preparation
-    // plus explosions later.
-    public void die ()
-    {
-        GameController gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-        gc.givePointsForKilling(this.transform.gameObject);
-        gc.deleteUnit(this.transform.gameObject);
-        
-        // This script is only available for player now.
-        gc.mothershipKilled (GameController.Who.Player);
-        foreach (Transform child in this.transform)
-        {
-            Destroy(child.gameObject);
-        }
-        Destroy(this.transform.gameObject);
+	
+	public void setHitPoints (float f)
+	{
+		this.hitPoints = f;
+	}
+	
+	// Called when unit reach below 0 hp.
+	// Destroys object and does some preparation
+	// plus explosions later.
+	public void die ()
+	{
+		GameController gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		gc.givePointsForKilling(this.transform.gameObject);
+		gc.deleteUnit(this.transform.gameObject);
+
+		// This script is only available for player now.
+		gc.mothershipKilled (GameController.Who.Player);
+		foreach (Transform child in this.transform)
+		{
+			Destroy(child.gameObject);
+		}
+		Destroy(this.transform.gameObject);
 	}
 }
